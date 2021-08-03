41e315c115faac7fa0f384a9799a6af81faff712
weightsforfun <giyoung914@gmail.com>
2021-08-03 20:40:35 +0900

    css is done

--- savejs/background.js
+++ savejs/background.js
@@ -1,5 +1,6 @@
-const images=["0.jpeg","1.jpeg"];
-const chosenImage=images[Math.floor(Math.random()*images.length)];
-const bgImage=document.createElement("img");
-bgImage.src=`img/${chosenImage}`;
+const images = ["river1.jpg", "bg1.jpeg", "bg2.jpeg", "bg3.jpeg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+bgImage.classList.add("mainbg");
 document.body.prepend(bgImage);
--- savejs/greeting.js
+++ savejs/greeting.js
@@ -1,24 +1,25 @@
-  
-const logininput =document.querySelector("#login-form input");
-const loginForm=document.querySelector("#login-form");
-const link=document.querySelector("a");
-const greeting=document.querySelector("#greeting");
-const HIDDEN_CLASSNAME="hidden";
-const USERNAME_KEY="username";
-function onLgoinSubmit(event){
-    event.preventDefault();
-    const username=logininput.value;
-    localStorage.setItem(USERNAME_KEY,username);
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText=`hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+const logininput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const greeting2 = document.querySelector("#greeting2");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+function onLgoinSubmit(event) {
+  event.preventDefault();
+  const username = logininput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-const savedUsername =localStorage.getItem(USERNAME_KEY);
-if(savedUsername===null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit",onLgoinSubmit);
-}
-else{
-    greeting.innerText=`hello ${savedUsername}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLgoinSubmit);
+} else {
+  greeting.innerText = `hello ${savedUsername}`;
+  greeting2.innerText = "what is your main focus for today?";
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting2.classList.remove(HIDDEN_CLASSNAME);
 }
