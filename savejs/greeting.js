7c1084850fc69b748af4244641bf2745253ebcb2
weightsforfun <giyoung914@gmail.com>
2021-08-04 14:47:44 +0900

    20210804 fix css before login

--- savejs/greeting.js
+++ savejs/greeting.js
@@ -3,6 +3,7 @@ const loginForm = document.querySelector("#login-form");
 const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 const greeting2 = document.querySelector("#greeting2");
+const todoList = document.querySelector("#todo-form");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 function onLgoinSubmit(event) {
@@ -12,6 +13,7 @@ function onLgoinSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   greeting.innerText = `hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  todoList.classList.remove(HIDDEN_CLASSNAME);
 }
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
@@ -22,4 +24,5 @@ if (savedUsername === null) {
   greeting2.innerText = "what is your main focus for today?";
   greeting.classList.remove(HIDDEN_CLASSNAME);
   greeting2.classList.remove(HIDDEN_CLASSNAME);
+  todoList.classList.remove(HIDDEN_CLASSNAME);
 }
